cb53b43823fad80789803b0cf3ec19ffccafdbd7
Kaelin Colclasure <kaelin@acm.org>
2013-04-06 10:53:05 -0700

    Renamed -test_vMAT_linkage to -test_normaldata_10x3_13.

--- vMATTests/ClusterTests.mm
+++ vMATTests/ClusterTests.mm
@@ -13,30 +13,32 @@
 
 @implementation ClusterTests
 
-- (void)test_vMAT_linkage;
+- (void)test_normaldata_10x3_13;
 {
     NSURL * URL = [[NSBundle bundleForClass:[self class]] URLForResource:@"cluster-normaldata-10x3-13"
                                                            withExtension:@"mat"];
     NSInputStream * stream = [NSInputStream inputStreamWithURL:URL];
     [stream open];
     dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
-    __block vMAT_Array * matX = nil;
-    __block vMAT_Array * matYv = nil;
-    __block vMAT_Array * matZv = nil;
-    vMAT_load(stream, @[@"X", @"Y", @"Zv"], ^(NSDictionary * workspace, NSError * error) {
-        // NSLog(@"%@", workspace);
-        matX =  [workspace variable:@"X"].matrix.mtrans;  // Transposed for vMAT_pdist
-        matYv = [workspace variable:@"Y"].matrix.mtrans;  // Transposed for vMAT_pdist
-        matZv = [workspace variable:@"Zv"].matrix.mtrans; // Transposed for vMAT_linkage
-        STAssertNotNil(matX, nil);
-        STAssertNotNil(matYv, nil);
-        STAssertNotNil(matZv, nil);
+    __block NSDictionary * ws = nil;
+    vMAT_load(stream, @[@"X", @"Y", @"Zv", @"W", @"Vcpt5", @"Vmax4"], ^(NSDictionary * workspace, NSError * error) {
+        NSLog(@"%@", workspace);
+        ws = workspace;
         [stream close];
         dispatch_semaphore_signal(semaphore);
     });
     long timedout = dispatch_semaphore_wait(semaphore, dispatch_time(DISPATCH_TIME_NOW,
                                                                      1 * NSEC_PER_SEC));
     STAssertFalse(timedout, @"Timed out waiting for completion (1s)");
+    vMAT_Array * matX = nil;
+    vMAT_Array * matYv = nil;
+    vMAT_Array * matZv = nil;
+    matX =  [ws variable:@"X"].matrix.mtrans;  // Transposed for vMAT_pdist
+    matYv = [ws variable:@"Y"].matrix.mtrans;  // Transposed for vMAT_pdist
+    matZv = [ws variable:@"Zv"].matrix.mtrans; // Transposed for vMAT_linkage
+    STAssertNotNil(matX, nil);
+    STAssertNotNil(matYv, nil);
+    STAssertNotNil(matZv, nil);
     // NSLog(@"X  = %@", matX);
     // NSLog(@"Yv = %@", matYv);
     // NSLog(@"Zv = %@", matZv.dump);
