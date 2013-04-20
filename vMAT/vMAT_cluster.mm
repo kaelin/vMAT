d235f20bcfe91983754e90c8edf85dfdd246141e
Kaelin Colclasure <kaelin@acm.org>
2013-04-19 20:41:16 -0700

    Temporarily transpose Z (twice) and Y so we can work down columns instead of across rows.

--- vMAT/vMAT_cluster.mm
+++ vMAT/vMAT_cluster.mm
@@ -20,9 +20,9 @@ namespace {
     using namespace std;
     using namespace vMAT;
     
-    typedef Mat<double, 3, Dynamic> MatZ; // Input is a 3xN hierarchical cluster tree
-    typedef Mat<double, 4, Dynamic> MatY; // Result is a 4x(N+1) inconsistancy matrix
-    typedef Mat<double> MatA;             // Result is Mx(N+1) assignment matrix
+    typedef Mat<double, Dynamic, 3> MatZ; // Input is a Mx3 hierarchical cluster tree (after Z gets transposed!)
+    typedef Mat<double, Dynamic, 4> MatY; // Result is a (M+1)x4 inconsistancy matrix (after Y gets transposed!)
+    typedef Mat<double> MatA;             // Result is (M+1)xN assignment matrix (after Z gets transposed!)
     
     struct Options {
         BOOL useCutoff;
@@ -39,26 +39,30 @@ namespace {
         return opts;
     }
     
+    typedef Array<bool, Dynamic, 1> ArrayX1b;
+    typedef vMAT::Map<ArrayX1b> MatArrayX1b;
+    typedef Mat<double, Dynamic, 1> MatX1d;
+    
     vMAT_Array *
     checkCut(MatZ Z, double cutoff, VectorXd crit)
     {
-        double n = Z.size(1);               // Indexes are zero-based, so no need to add one
-        vMAT::Map<Array<bool, Dynamic, 1>> conn = (crit.array() < cutoff).eval();
-        Array<bool, Dynamic, 1> notLeaf = Z.array().row(0) > n || Z.array().row(1) > n;
-        vMAT::Map<Array<bool, Dynamic, 1>> todo = (conn && notLeaf).eval();
+        double n = Z.size(0);               // Indexes are zero-based, so no need to add one
+        MatArrayX1b conn = (crit.array() < cutoff).eval();
+        ArrayX1b notLeaf = Z.array().col(0) > n || Z.array().col(1) > n;
+        MatArrayX1b todo = (conn && notLeaf).eval();
         while(todo.any()) {
             Mat<vMAT_idx_t, Dynamic, 1> rows = vMAT_find(todo.matA, nil);
             Mat<bool, Dynamic, 2> cdone = vMAT_ones(vMAT_MakeSize(rows.size(), 2), @[ @"logical" ]);
             for (vMAT_idx_t j : { 0, 1 }) { // 0 is left child, 1 is right child
-                Mat<double, Dynamic, 1> crows = vMAT_pick(Z.matA, @[ [NSNumber numberWithLong:j], rows.matA ]).mtrans;
+                MatX1d crows = vMAT_pick(Z.matA, @[ rows.matA, [NSNumber numberWithLong:j] ]);
                 cerr << "crows = " << crows << endl;
-                Array<bool, Dynamic, 1> t = (crows.array() > n);
+                ArrayX1b t = (crows.array() > n);
                 vMAT_Array * matT = vMAT_cast(t);
                 if (t.any()) {
-                    Mat<double, Dynamic, 1> child = vMAT_pick(crows.matA, @[ matT ]);
+                    MatX1d child = vMAT_pick(crows.matA, @[ matT ]);
                     child.array() -= n;
-                    vMAT::Map<Array<bool, Dynamic, 1>> childTodo = vMAT_pick(todo.matA, @[ child.matA ]);
-                    vMAT::Map<Array<bool, Dynamic, 1>> childNotTodo = childTodo.unaryExpr([](bool elt) { return !elt; }).eval();
+                    MatArrayX1b childTodo = vMAT_pick(todo.matA, @[ child.matA ]);
+                    MatArrayX1b childNotTodo = childTodo.unaryExpr([](bool elt) { return !elt; }).eval();
                     vMAT_place(cdone.matA, @[ matT, [NSNumber numberWithLong:j] ], childNotTodo.matA);
                     NSLog(@"%@", cdone.matA.dump);
                 }
@@ -76,18 +80,18 @@ vMAT_cluster(vMAT_Array * matZ,
 {
     Options opts = clusterOptions(options);
     
-    MatZ Z = vMAT_double(matZ);
-    vMAT_idx_t n = Z.size(1) + 1;
+    MatZ Z = vMAT_double(matZ.mtrans); // Note mtrans!
+    vMAT_idx_t m = Z.size(0) + 1;
     if (opts.useCutoff) {
-        vMAT_idx_t m = static_cast<vMAT_idx_t>(opts.cutoff.size());
-        VectorXd crit(n - 1);
+        vMAT_idx_t n = static_cast<vMAT_idx_t>(opts.cutoff.size());
+        VectorXd crit(m - 1);
         MatA A = vMAT_zeros(vMAT_MakeSize(m, n), nil);
         if (opts.useInconsistent) {
-            MatY Y = vMAT_inconsistent(Z, opts.depth);
-            crit = Y.row(3);
+            MatY Y = vMAT_inconsistent(Z.matA.mtrans, opts.depth).mtrans; // Note double mtrans!
+            crit = Y.col(3);
         }
         else {
-            crit = Z.row(2);
+            crit = Z.col(2);
         }
         for (auto cutoff : opts.cutoff) {
             vMAT_Array * matC = checkCut(Z, cutoff, crit);
