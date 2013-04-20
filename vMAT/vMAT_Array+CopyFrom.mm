1a6b8ce489d87b74231fd604c44d60d5364e40de
Kaelin Colclasure <kaelin@acm.org>
2013-04-19 20:12:51 -0700

    Implemented all of the vMAT_Array (CopyFrom) methods (using a ruby script).

--- vMAT/vMAT_Array+CopyFrom.mm
+++ vMAT/vMAT_Array+CopyFrom.mm
@@ -54,70 +54,310 @@ namespace {
         for (vMAT_idx_t i = 0;
              i < lenA;
              i++) {
-            A[i] = B[i];
+            A[i] = static_cast<TypeA>(B[i]);
         }
     }
 
 }
 
-- (void)_copy_miDOUBLE_from_miDOUBLE:(vMAT_Array *)matrix;
+- (void)_copy_miINT8_from_miINT8:(vMAT_Array *)matrix;
 {
-    copyFrom(self, matrix, DOUBLE, DOUBLE);
+    copyFrom(self, matrix, INT8, INT8);
 }
 
-- (void)_copy_miDOUBLE_from_miSINGLE:(vMAT_Array *)matrix;
+- (void)_copy_miINT8_from_miUINT8:(vMAT_Array *)matrix;
 {
-    copyFrom(self, matrix, DOUBLE, SINGLE);
+    copyFrom(self, matrix, INT8, UINT8);
 }
 
-- (void)_copy_miDOUBLE_from_miINT8:(vMAT_Array *)matrix;
+- (void)_copy_miINT8_from_miINT16:(vMAT_Array *)matrix;
 {
-    copyFrom(self, matrix, DOUBLE, INT8);
+    copyFrom(self, matrix, INT8, INT16);
 }
 
-- (void)_copy_miDOUBLE_from_miUINT8:(vMAT_Array *)matrix;
+- (void)_copy_miINT8_from_miUINT16:(vMAT_Array *)matrix;
 {
-    copyFrom(self, matrix, DOUBLE, UINT8);
+    copyFrom(self, matrix, INT8, UINT16);
 }
 
-- (void)_copy_miDOUBLE_from_miINT16:(vMAT_Array *)matrix;
+- (void)_copy_miINT8_from_miINT32:(vMAT_Array *)matrix;
 {
-    copyFrom(self, matrix, DOUBLE, INT16);
+    copyFrom(self, matrix, INT8, INT32);
 }
 
-- (void)_copy_miDOUBLE_from_miUINT16:(vMAT_Array *)matrix;
+- (void)_copy_miINT8_from_miUINT32:(vMAT_Array *)matrix;
 {
-    copyFrom(self, matrix, DOUBLE, UINT16);
+    copyFrom(self, matrix, INT8, UINT32);
 }
 
-- (void)_copy_miDOUBLE_from_miINT32:(vMAT_Array *)matrix;
+- (void)_copy_miINT8_from_miSINGLE:(vMAT_Array *)matrix;
 {
-    copyFrom(self, matrix, DOUBLE, INT32);
+    copyFrom(self, matrix, INT8, SINGLE);
 }
 
-- (void)_copy_miDOUBLE_from_miUINT32:(vMAT_Array *)matrix;
+- (void)_copy_miINT8_from_miDOUBLE:(vMAT_Array *)matrix;
 {
-    copyFrom(self, matrix, DOUBLE, UINT32);
+    copyFrom(self, matrix, INT8, DOUBLE);
 }
 
-- (void)_copy_miDOUBLE_from_miINT64:(vMAT_Array *)matrix;
+- (void)_copy_miINT8_from_miINT64:(vMAT_Array *)matrix;
 {
-    copyFrom(self, matrix, DOUBLE, INT64);
+    copyFrom(self, matrix, INT8, INT64);
 }
 
-- (void)_copy_miDOUBLE_from_miUINT64:(vMAT_Array *)matrix;
+- (void)_copy_miINT8_from_miUINT64:(vMAT_Array *)matrix;
 {
-    copyFrom(self, matrix, DOUBLE, UINT64);
+    copyFrom(self, matrix, INT8, UINT64);
 }
 
-- (void)_copy_miSINGLE_from_miDOUBLE:(vMAT_Array *)matrix;
+- (void)_copy_miUINT8_from_miINT8:(vMAT_Array *)matrix;
 {
-    copyFrom(self, matrix, SINGLE, DOUBLE);
+    copyFrom(self, matrix, UINT8, INT8);
 }
 
-- (void)_copy_miSINGLE_from_miSINGLE:(vMAT_Array *)matrix;
+- (void)_copy_miUINT8_from_miUINT8:(vMAT_Array *)matrix;
 {
-    copyFrom(self, matrix, SINGLE, SINGLE);
+    copyFrom(self, matrix, UINT8, UINT8);
+}
+
+- (void)_copy_miUINT8_from_miINT16:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT8, INT16);
+}
+
+- (void)_copy_miUINT8_from_miUINT16:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT8, UINT16);
+}
+
+- (void)_copy_miUINT8_from_miINT32:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT8, INT32);
+}
+
+- (void)_copy_miUINT8_from_miUINT32:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT8, UINT32);
+}
+
+- (void)_copy_miUINT8_from_miSINGLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT8, SINGLE);
+}
+
+- (void)_copy_miUINT8_from_miDOUBLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT8, DOUBLE);
+}
+
+- (void)_copy_miUINT8_from_miINT64:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT8, INT64);
+}
+
+- (void)_copy_miUINT8_from_miUINT64:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT8, UINT64);
+}
+
+- (void)_copy_miINT16_from_miINT8:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT16, INT8);
+}
+
+- (void)_copy_miINT16_from_miUINT8:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT16, UINT8);
+}
+
+- (void)_copy_miINT16_from_miINT16:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT16, INT16);
+}
+
+- (void)_copy_miINT16_from_miUINT16:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT16, UINT16);
+}
+
+- (void)_copy_miINT16_from_miINT32:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT16, INT32);
+}
+
+- (void)_copy_miINT16_from_miUINT32:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT16, UINT32);
+}
+
+- (void)_copy_miINT16_from_miSINGLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT16, SINGLE);
+}
+
+- (void)_copy_miINT16_from_miDOUBLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT16, DOUBLE);
+}
+
+- (void)_copy_miINT16_from_miINT64:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT16, INT64);
+}
+
+- (void)_copy_miINT16_from_miUINT64:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT16, UINT64);
+}
+
+- (void)_copy_miUINT16_from_miINT8:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT16, INT8);
+}
+
+- (void)_copy_miUINT16_from_miUINT8:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT16, UINT8);
+}
+
+- (void)_copy_miUINT16_from_miINT16:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT16, INT16);
+}
+
+- (void)_copy_miUINT16_from_miUINT16:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT16, UINT16);
+}
+
+- (void)_copy_miUINT16_from_miINT32:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT16, INT32);
+}
+
+- (void)_copy_miUINT16_from_miUINT32:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT16, UINT32);
+}
+
+- (void)_copy_miUINT16_from_miSINGLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT16, SINGLE);
+}
+
+- (void)_copy_miUINT16_from_miDOUBLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT16, DOUBLE);
+}
+
+- (void)_copy_miUINT16_from_miINT64:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT16, INT64);
+}
+
+- (void)_copy_miUINT16_from_miUINT64:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT16, UINT64);
+}
+
+- (void)_copy_miINT32_from_miINT8:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT32, INT8);
+}
+
+- (void)_copy_miINT32_from_miUINT8:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT32, UINT8);
+}
+
+- (void)_copy_miINT32_from_miINT16:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT32, INT16);
+}
+
+- (void)_copy_miINT32_from_miUINT16:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT32, UINT16);
+}
+
+- (void)_copy_miINT32_from_miINT32:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT32, INT32);
+}
+
+- (void)_copy_miINT32_from_miUINT32:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT32, UINT32);
+}
+
+- (void)_copy_miINT32_from_miSINGLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT32, SINGLE);
+}
+
+- (void)_copy_miINT32_from_miDOUBLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT32, DOUBLE);
+}
+
+- (void)_copy_miINT32_from_miINT64:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT32, INT64);
+}
+
+- (void)_copy_miINT32_from_miUINT64:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT32, UINT64);
+}
+
+- (void)_copy_miUINT32_from_miINT8:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT32, INT8);
+}
+
+- (void)_copy_miUINT32_from_miUINT8:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT32, UINT8);
+}
+
+- (void)_copy_miUINT32_from_miINT16:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT32, INT16);
+}
+
+- (void)_copy_miUINT32_from_miUINT16:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT32, UINT16);
+}
+
+- (void)_copy_miUINT32_from_miINT32:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT32, INT32);
+}
+
+- (void)_copy_miUINT32_from_miUINT32:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT32, UINT32);
+}
+
+- (void)_copy_miUINT32_from_miSINGLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT32, SINGLE);
+}
+
+- (void)_copy_miUINT32_from_miDOUBLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT32, DOUBLE);
+}
+
+- (void)_copy_miUINT32_from_miINT64:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT32, INT64);
+}
+
+- (void)_copy_miUINT32_from_miUINT64:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT32, UINT64);
 }
 
 - (void)_copy_miSINGLE_from_miINT8:(vMAT_Array *)matrix;
@@ -150,6 +390,16 @@ namespace {
     copyFrom(self, matrix, SINGLE, UINT32);
 }
 
+- (void)_copy_miSINGLE_from_miSINGLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, SINGLE, SINGLE);
+}
+
+- (void)_copy_miSINGLE_from_miDOUBLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, SINGLE, DOUBLE);
+}
+
 - (void)_copy_miSINGLE_from_miINT64:(vMAT_Array *)matrix;
 {
     copyFrom(self, matrix, SINGLE, INT64);
@@ -158,6 +408,156 @@ namespace {
 - (void)_copy_miSINGLE_from_miUINT64:(vMAT_Array *)matrix;
 {
     copyFrom(self, matrix, SINGLE, UINT64);
+}
+
+- (void)_copy_miDOUBLE_from_miINT8:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, DOUBLE, INT8);
+}
+
+- (void)_copy_miDOUBLE_from_miUINT8:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, DOUBLE, UINT8);
+}
+
+- (void)_copy_miDOUBLE_from_miINT16:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, DOUBLE, INT16);
+}
+
+- (void)_copy_miDOUBLE_from_miUINT16:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, DOUBLE, UINT16);
+}
+
+- (void)_copy_miDOUBLE_from_miINT32:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, DOUBLE, INT32);
+}
+
+- (void)_copy_miDOUBLE_from_miUINT32:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, DOUBLE, UINT32);
+}
+
+- (void)_copy_miDOUBLE_from_miSINGLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, DOUBLE, SINGLE);
+}
+
+- (void)_copy_miDOUBLE_from_miDOUBLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, DOUBLE, DOUBLE);
+}
+
+- (void)_copy_miDOUBLE_from_miINT64:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, DOUBLE, INT64);
+}
+
+- (void)_copy_miDOUBLE_from_miUINT64:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, DOUBLE, UINT64);
+}
+
+- (void)_copy_miINT64_from_miINT8:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT64, INT8);
+}
+
+- (void)_copy_miINT64_from_miUINT8:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT64, UINT8);
+}
+
+- (void)_copy_miINT64_from_miINT16:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT64, INT16);
+}
+
+- (void)_copy_miINT64_from_miUINT16:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT64, UINT16);
+}
+
+- (void)_copy_miINT64_from_miINT32:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT64, INT32);
+}
+
+- (void)_copy_miINT64_from_miUINT32:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT64, UINT32);
+}
+
+- (void)_copy_miINT64_from_miSINGLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT64, SINGLE);
+}
+
+- (void)_copy_miINT64_from_miDOUBLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT64, DOUBLE);
+}
+
+- (void)_copy_miINT64_from_miINT64:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT64, INT64);
+}
+
+- (void)_copy_miINT64_from_miUINT64:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, INT64, UINT64);
+}
+
+- (void)_copy_miUINT64_from_miINT8:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT64, INT8);
+}
+
+- (void)_copy_miUINT64_from_miUINT8:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT64, UINT8);
+}
+
+- (void)_copy_miUINT64_from_miINT16:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT64, INT16);
+}
+
+- (void)_copy_miUINT64_from_miUINT16:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT64, UINT16);
+}
+
+- (void)_copy_miUINT64_from_miINT32:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT64, INT32);
+}
+
+- (void)_copy_miUINT64_from_miUINT32:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT64, UINT32);
+}
+
+- (void)_copy_miUINT64_from_miSINGLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT64, SINGLE);
+}
+
+- (void)_copy_miUINT64_from_miDOUBLE:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT64, DOUBLE);
+}
+
+- (void)_copy_miUINT64_from_miINT64:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT64, INT64);
+}
+
+- (void)_copy_miUINT64_from_miUINT64:(vMAT_Array *)matrix;
+{
+    copyFrom(self, matrix, UINT64, UINT64);
 }
 
 @end
